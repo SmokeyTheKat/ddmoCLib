a00ec533aa8b9f071435d239fe5693a2053cd86a
= <owr5273@gmail.com>
2020-10-03 20:58:11 -0300

    added ddcCharSets

---
+++ ddcCharSets/ddcCharSets.h
@@ -0,0 +1,18 @@
+#ifndef __ddcCharSets__
+#define __ddcCharSets__
+
+const char* cset_lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const char* cset_upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char* cset_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+const char* cset_block = "█";
+const char* cset_mediumBlock = "▒";
+const char* cset_lightBlock = "░";
+const char* cset_darkBlock = "▓";
+const char* cset_leftBar = "▕";
+const char* cset_rightBar = "▏";
+const char* cset_lowerBar = "▁";
+
+
+#endif
+
