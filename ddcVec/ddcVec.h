5c0a375fc851c85c2ec4ba70c986dd3f539c14cb
= <owr5273@gmail.com>
2020-11-18 15:23:12 -0400

    added ddIVec2

--- ddcVec/ddcVec.h
+++ ddcVec/ddcVec.h
@@ -5,6 +5,7 @@
 #include <ddcDef.h>
 
 typedef struct ddVec2 ddVec2;
+typedef struct ddIVec2 ddIVec2;
 typedef struct ddVec3 ddVec3;
 
 
@@ -22,6 +23,17 @@ double  ddVec2_magnitude(ddVec2 _d);
 ddVec2  ddVec2_normalize(ddVec2 _d);
 
 
+ddIVec2 make_ddIVec2(int _x, int _y);
+
+void    ddIVec2_set(ddIVec2* _d, const ddIVec2 _s);
+bool    ddIVec2_compare(const ddIVec2 _d, const ddIVec2 _s);
+void    ddIVec2_add(ddIVec2* _d, const ddIVec2 _s);
+void    ddIVec2_subtract(ddIVec2* _d, const ddIVec2 _s);
+void    ddIVec2_multiply(ddIVec2* _d, const ddIVec2 _s);
+void    ddIVec2_divide(ddIVec2* _d, const ddIVec2 _s);
+void    ddIVec2_dot(ddIVec2* _d, const ddIVec2 _s);
+double  ddIVec2_magnitude(ddIVec2 _d);
+ddIVec2  ddIVec2_normalize(ddIVec2 _d);
 
 ddVec3 make_ddVec3(double _x, double _y, double _z);
 
@@ -44,14 +56,18 @@ struct ddVec2
 	double y;
 };
 
+struct ddIVec2
+{
+	int x;
+	int y;
+};
+
 struct ddVec3
 {
 	double x;
 	double y;
 	double z;
 };
-
-
 
 
 ddVec2 make_ddVec2(double _x, double _y)
@@ -101,7 +117,7 @@ void ddVec2_dot(ddVec2* _d, const ddVec2 _s)
 }
 double ddVec2_magnitude(ddVec2 _d)
 {
-	return sqrt(pow(_d.x, 2) + pow(_d.y, 2));
+	return sqrt((_d.x*_d.x) + (_d.y*_d.y));
 }
 ddVec2 ddVec2_normalize(ddVec2 _d)
 {
@@ -111,6 +127,86 @@ ddVec2 ddVec2_normalize(ddVec2 _d)
 	_o.y = _d.y / mag;
 	return _o;
 }
+
+
+
+
+ddIVec2 make_ddIVec2(int _x, int _y)
+{
+	ddIVec2 _o;
+	_o.x = _x;
+	_o.y = _y;
+	return _o;
+}
+
+
+bool ddIVec2_compare(const ddIVec2 _d, const ddIVec2 _s)
+{
+	return ((_d.x == _s.x) && (_d.y == _s.y));
+}
+void ddIVec2_set(ddIVec2* _d, const ddIVec2 _s)
+{
+	_d->x = _s.x;
+	_d->y = _s.y;
+}
+
+
+void ddIVec2_add(ddIVec2* _d, const ddIVec2 _s)
+{
+	_d->x += _s.x;
+	_d->y += _s.y;
+}
+void ddIVec2_subtract(ddIVec2* _d, const ddIVec2 _s)
+{
+	_d->x -= _s.x;
+	_d->y -= _s.y;
+}
+void ddIVec2_multiply(ddIVec2* _d, const ddIVec2 _s)
+{
+	_d->x *= _s.x;
+	_d->y *= _s.y;
+}
+void ddIVec2_divide(ddIVec2* _d, const ddIVec2 _s)
+{
+	_d->x = (int)((double)_d->x / (double)_s.x);
+	_d->y = (int)((double)_d->x / (double)_s.y);
+}
+void ddIVec2_dot(ddIVec2* _d, const ddIVec2 _s)
+{
+	_d->x *= _s.x;
+	_d->y *= _s.y;
+}
+double ddIVec2_magnitude(ddIVec2 _d)
+{
+	return sqrt((_d.x*_d.x) + (_d.y*_d.y));
+}
+ddIVec2 ddIVec2_normalize(ddIVec2 _d)
+{
+	ddIVec2 _o;
+	double mag = ddIVec2_magnitude(_d);
+	_o.x = (int)((double)_d.x / mag);
+	_o.y = (int)((double)_d.y / mag);
+	return _o;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -158,7 +254,7 @@ void ddVec3_dot(ddVec3* _d, const ddVec3 _s)
 }
 double ddVec3_magnitude(ddVec3 _d)
 {
-	return (double)sqrt(pow(_d.x, 2) + pow(_d.y, 2) + pow(_d.z, 2));
+	return sqrt((_d.x*_d.x) + (_d.y*_d.y) + (_d.z*_d.z));
 }
 ddVec3 ddVec3_normalize(ddVec3 _d)
 {
