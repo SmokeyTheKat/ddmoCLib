c4a3625a3d4b19aa50bee2ceeb727dab40c0dd71
smokeythekat <owr5273@gmail.com>
2021-03-11 21:52:32 -0400

    removed semi

--- ddcChars.h
+++ ddcChars.h
@@ -6,21 +6,21 @@ const char* cset_upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const char* cset_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const char* cset_numbers = "0123456789";
 
-#define CS_BLOCK "█";
-#define CS_MBLOCK "▒";
-#define CS_LBLOCK "░";
-#define CS_DBLOCK "▓";
-#define CS_LBAR "▕";
-#define CS_RBAR "▏";
-#define CS_BBAR "▁";
-#define CS_VBAR "│";
-#define CS_HBAR "─";
-#define CS_TRBAR "┐";
-#define CS_TLBAR "┌";
-#define CS_BRBAR "┘";
-#define CS_BLBAR "└";
-#define CS_LSTOP "├";
-#define CS_RSTOP "┤";
+#define CS_BLOCK "█"
+#define CS_MBLOCK "▒"
+#define CS_LBLOCK "░"
+#define CS_DBLOCK "▓"
+#define CS_LBAR "▕"
+#define CS_RBAR "▏"
+#define CS_BBAR "▁"
+#define CS_VBAR "│"
+#define CS_HBAR "─"
+#define CS_TRBAR "┐"
+#define CS_TLBAR "┌"
+#define CS_BRBAR "┘"
+#define CS_BLBAR "└"
+#define CS_LSTOP "├"
+#define CS_RSTOP "┤"
 
 
 #endif
