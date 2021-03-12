9d063312cf74dc97c92cdb1055445317abb5116c
smokeythekat <owr5273@gmail.com>
2021-03-11 22:48:14 -0400

    byte conversions

--- ddcBits.h
+++ ddcBits.h
@@ -9,4 +9,13 @@ typedef unsigned char byte;
 #define bit_tog(x, p) (x ^ (1 << p))
 #define bit_setv(x, v, p) ((v == 1) ? bit_set(x, p) : bit_unset(x, p))
 
+#define TB_to_B(x) (x*1000000000000)
+#define B_to_TB(x) (x/1000000000000)
+#define GB_to_B(x) (x*1000000000)
+#define B_to_GB(x) (x/1000000000)
+#define MB_to_B(x) (x*1000000)
+#define B_to_MB(x) (x/1000000)
+#define KB_to_B(x) (x*1000)
+#define B_to_KB(x) (x/1000)
+
 #endif
