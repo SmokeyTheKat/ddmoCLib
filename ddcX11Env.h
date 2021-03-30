b95f9e792efe3799d5d4233527093fa4331258cf
SmokeyTheKat <owr5273@gmail.com>
2021-03-29 23:30:48 -0300

    mouse click

--- ddcX11Env.h
+++ ddcX11Env.h
@@ -39,6 +39,10 @@ void ddXE_set_pixel(int x, int y)
 {
 	XDrawPoint(d, w, gc, x, y);
 }
+void ddXE_draw_line(int x1, int y1, int x2, int y2)
+{
+	XDrawLine(d, w, gc, x1, y1, x2, y2);
+}
 void ddXE_draw_text(int x, int y, char* str, int length)
 {
 	XDrawString(d, w, gc, x, y, str, length);
@@ -52,7 +56,7 @@ void ddXE_draw_bitmap(int x, int y, char* filename)
 	XCopyArea(d, pm, w, gc, 0, 0, width, height, x, y);
 }
 
-int ddXE_start(void(*fdraw)(void), void(*fkeypress)(char), char* running)
+int ddXE_start(void(*fdraw)(void), void(*fkeypress)(char), void(*fmouseclick)(int,int,int), char* running)
 {
 	d = XOpenDisplay(NULL);
 	if (d == NULL)
@@ -63,20 +67,39 @@ int ddXE_start(void(*fdraw)(void), void(*fkeypress)(char), char* running)
 	w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
 					BlackPixel(d, s), WhitePixel(d, s));
 	gc = DefaultGC(d, s);
-	XSelectInput(d, w, ExposureMask | KeyPressMask);
+	XSelectInput(d, w, ExposureMask | KeyPressMask | ButtonPressMask | ButtonReleaseMask);
 	XMapWindow(d, w);
 
 	while (1)
 	{
 		if (!(*running)) break;
 		XNextEvent(d, &e);
-		if (e.type == Expose)
+		switch (e.type)
 		{
-			fdraw();
-		}
-		if (e.type == KeyPress)
-		{
-			fkeypress(XLookupKeysym(&(e.xkey), 0));
+			case Expose:
+				fdraw();
+				break;
+			case KeyPress:
+				fkeypress(XLookupKeysym(&(e.xkey), 0));
+				break;
+			case ButtonPress:
+			{
+				switch (e.xbutton.button)
+				{
+					case Button1:
+						fmouseclick(1, e.xbutton.x, e.xbutton.y);
+						break;
+					case Button2:
+						fmouseclick(2, e.xbutton.x, e.xbutton.y);
+						break;
+					case Button3:
+						fmouseclick(3, e.xbutton.x, e.xbutton.y);
+						break;
+					default:
+						fmouseclick(-1, e.xbutton.x, e.xbutton.y);
+						break;
+				}
+			} break;
 		}
 	}
 
