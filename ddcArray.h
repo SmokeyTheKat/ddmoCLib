27082b2821c99f06508856d9238b80891dd4de29
smokeythekat <owr5273@gmail.com>
2021-06-07 07:53:28 -0300

    yo

--- ddcArray.h
+++ ddcArray.h
@@ -1,6 +1,8 @@
 #ifndef __ddcArray__
 #define __ddcArray__
 
+#include <malloc.h>
+
 typedef struct
 {
 	void* data;
@@ -9,28 +11,35 @@ typedef struct
 } ddArray;
 
 #define make_ddArray(s, c) { \
-		(s)->data = malloc(c); \
-		(s)->len = 0; \
-		(s)->cap = c; \
+		(s).data = malloc(c); \
+		(s).len = 0; \
+		(s).cap = c; \
 	}
 #define ddArray_push(s, v, t) { \
-		if ((s)->len + sizeof(t) >= (s)->cap) \
-			realloc((s)->data, (s)->len+(2*sizeof(t))); \
-		*((t*)(&((char*)(s)->data)[(s)->len])) = (t)(v); \
-		(s)->len += sizeof(t); \
+		if ((s).len + sizeof(t) + 1 >= (s).cap) \
+		{ \
+			(s).cap += 10*sizeof(t); \
+			char* __new_v_data_l = malloc((s).cap); \
+			for (long __vi_l = 0; __vi_l < (s).len; __vi_l++) \
+				__new_v_data_l[__vi_l] = ((char*)(s).data)[__vi_l]; \
+			free((s).data); \
+			(s).data = __new_v_data_l; \
+		} \
+		*((t*)(&((char*)(s).data)[(s).len])) = (t)(v); \
+		(s).len += sizeof(t); \
 	}
 #define ddArray_pop(s, o, t) { \
-		*o = *(t*)(&(((char*)((s)->data))[(s)->len-sizeof(t)])); \
-		(s)->len -= sizeof(t); \
+		*o = *(t*)(&(((char*)((s).data))[(s).len-sizeof(t)])); \
+		(s).len -= sizeof(t); \
 	}
 #define ddArray_set(s, v, i, t) { \
-		*(t*)(&(((char*)((s)->data))[i*sizeof(t)])) = v; \
+		*(t*)(&(((char*)((s).data))[i*sizeof(t)])) = v; \
 	}
 #define ddArray_get(s, i, t) \
-		(*(t*)(&(((char*)((s)->data))[i*sizeof(t)])))
+		(*(t*)(&(((char*)((s).data))[i*sizeof(t)])))
 #define ddArray_length(s, t) \
-		((s)->len/sizeof(t))
+		((s).len/sizeof(t))
 #define ddArray_top(s, v, t) \
-		(*(t*)(&(((char*)((s)->data))[(s)->len-sizeof(t)])))
+		(*(t*)(&(((char*)((s).data))[(s).len-sizeof(t)])))
 
 #endif
--- ddcSocket.h
+++ ddcSocket.h
@@ -8,23 +8,115 @@
 
 #define __DSOCKET_BUFFER_SIZE 1024
 
-struct dsocketClient;
-struct dsocketServer;
+struct dsocket_udp_client;
+struct dsocket_udp_server;
 
-struct dsocketServer make_dsocketServer(int port);
-int dsocketServer_bind(struct dsocketServer* sck);
-int dsocketServer_start_listen(struct dsocketServer* sck);
-int dsocketServer_listen(struct dsocketServer* sck);
-int dsocketServer_send(struct dsocketServer sck, int client, const char* data, long length);
-int dsocketServer_receive(struct dsocketServer sck, int client, char* buffer, long length);
+struct dsocket_udp_server make_dsocket_udp_server(int port);
+int dsocket_udp_server_bind(struct dsocket_udp_server* sck);
+struct sockaddr_in dsocket_udp_server_receive(struct dsocket_udp_server sck, char* buffer, long* length, long max_length);
+int dsocket_udp_server_send(struct dsocket_udp_server sck, struct sockaddr_in client, char* buffer, long length);
 
-struct dsocketClient make_dsocketClient(char* addr, int port);
-void raze_dsocketClient(struct dsocketClient* sck);
-int dsocketClient_connect(struct dsocketClient* sck);
-int dsocketClient_send(struct dsocketClient sck, const char* data, long length);
-int dsocketClient_receive(struct dsocketClient sck, char* buffer, long length);
+struct dsocket_udp_client make_dsocket_udp_client(char* addr, int port);
+int dsocket_udp_client_connect(struct dsocket_udp_client* sck);
+int dsocket_udp_client_send(struct dsocket_udp_client sck, char* buffer, long length);
+int dsocket_udp_client_receive(struct dsocket_udp_client sck, char* buffer, long* length, long max_length);
 
-struct dsocketClient
+struct dsocket_udp_client
+{
+	int dscr;
+	int port;
+	char* addr;
+	struct sockaddr_in server;
+	int server_len;
+};
+
+struct dsocket_udp_server
+{
+	int dscr;
+	int port;
+	struct sockaddr_in server;
+	int server_len;
+};
+
+struct dsocket_udp_client make_dsocket_udp_client(char* addr, int port)
+{
+	struct dsocket_udp_client output;
+	output.addr = addr;
+	output.port = port;
+	return output;
+}
+int dsocket_udp_client_connect(struct dsocket_udp_client* sck)
+{
+	if ((sck->dscr = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+		return 1;
+	sck->server.sin_family = AF_INET;
+	sck->server.sin_port = htons(sck->port);
+	sck->server.sin_addr.s_addr = inet_addr(sck->addr);
+}
+
+int dsocket_udp_client_send(struct dsocket_udp_client sck, char* buffer, long length)
+{
+	sendto(sck.dscr, buffer, length, 0, (struct sockaddr*)&sck.server, sizeof(sck.server));
+}
+int dsocket_udp_client_receive(struct dsocket_udp_client sck, char* buffer, long* length, long max_length)
+{
+	int len;
+	*length = recvfrom(sck.dscr, buffer, max_length, MSG_WAITALL, (struct sockaddr*)&sck.server, &len);
+}
+
+struct dsocket_udp_server make_dsocket_udp_server(int port)
+{
+	struct dsocket_udp_server output;
+	output.server_len = sizeof(output.server);
+	output.port = port;
+	return output;
+}
+
+int dsocket_udp_server_bind(struct dsocket_udp_server* sck)
+{
+	if ((sck->dscr = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		return 1;
+	sck->server.sin_port = htons(sck->port);
+	sck->server.sin_family = AF_INET;
+	sck->server.sin_addr.s_addr = INADDR_ANY;
+	if (bind(sck->dscr, (struct sockaddr*)&sck->server, sizeof(sck->server)) < 0)
+		return 1;
+}
+
+int dsocket_udp_server_send(struct dsocket_udp_server sck, struct sockaddr_in client, char* buffer, long length)
+{
+	recvfrom(sck.dscr, buffer, length, MSG_CONFIRM, (struct sockaddr*)&client, sizeof(client));
+}
+
+struct sockaddr_in dsocket_udp_server_receive(struct dsocket_udp_server sck, char* buffer, long* length, long max_length)
+{
+	struct sockaddr_in client = {0};
+	int client_len = sizeof(client);
+	*length = recvfrom(sck.dscr, buffer, max_length, MSG_WAITALL, (struct sockaddr*)&client, &client_len);
+	return client;
+}
+
+
+
+
+
+struct dsocket_tcp_client;
+struct dsocket_tcp_server;
+
+struct dsocket_tcp_server make_dsocket_tcp_server(int port);
+int dsocket_tcp_server_bind(struct dsocket_tcp_server* sck);
+int dsocket_tcp_server_start_listen(struct dsocket_tcp_server* sck);
+int dsocket_tcp_server_listen(struct dsocket_tcp_server* sck);
+int dsocket_tcp_server_send(struct dsocket_tcp_server sck, int client, const char* data, long length);
+int dsocket_tcp_server_receive(struct dsocket_tcp_server sck, int client, char* buffer, long length);
+
+struct dsocket_tcp_client make_dsocket_tcp_client(char* addr, int port);
+void raze_dsocket_tcp_client(struct dsocket_tcp_client* sck);
+int dsocket_tcp_client_connect(struct dsocket_tcp_client* sck);
+int dsocket_tcp_client_send(struct dsocket_tcp_client sck, const char* data, long length);
+int dsocket_tcp_client_receive(struct dsocket_tcp_client sck, char* buffer, long length);
+
+struct dsocket_tcp_client
 {
 	int dscr;
 	int port;
@@ -32,7 +124,7 @@ struct dsocketClient
 	char* addr;
 };
 
-struct dsocketServer
+struct dsocket_tcp_server
 {
 	int dscr;
 	int port;
@@ -41,16 +133,16 @@ struct dsocketServer
 	int server_len;
 };
 
-struct dsocketServer make_dsocketServer(int port)
+struct dsocket_tcp_server make_dsocket_tcp_server(int port)
 {
-	struct dsocketServer output;
+	struct dsocket_tcp_server output;
 	output.server_len = sizeof(output.server);
 	output.port = port;
 	output.opt = 1;
 	return output;
 }
 
-int dsocketServer_bind(struct dsocketServer* sck)
+int dsocket_tcp_server_bind(struct dsocket_tcp_server* sck)
 {
 	if ((sck->dscr = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 		return 1;
@@ -63,44 +155,44 @@ int dsocketServer_bind(struct dsocketServer* sck)
 		return 1;
 	return 0;
 }
-int dsocketServer_start_listen(struct dsocketServer* sck)
+int dsocket_tcp_server_start_listen(struct dsocket_tcp_server* sck)
 {
 	if (listen(sck->dscr, 3) < 0)
 		return 1;
 	return 0;
 }
-int dsocketServer_listen(struct dsocketServer* sck)
+int dsocket_tcp_server_listen(struct dsocket_tcp_server* sck)
 {
 	int client_out = -1;
 	if ((client_out = accept(sck->dscr, (struct sockaddr*)&sck->server, (socklen_t*)&(sck->server_len))) < 0)
 		return -1;
 	return client_out;
 }
-int dsocketServer_send(struct dsocketServer sck, int client, const char* data, long length)
+int dsocket_tcp_server_send(struct dsocket_tcp_server sck, int client, const char* data, long length)
 {
 	send(client, data, length, 0);
 	return 1;
 }
-int dsocketServer_receive(struct dsocketServer sck, int client, char* buffer, long length)
+int dsocket_tcp_server_receive(struct dsocket_tcp_server sck, int client, char* buffer, long length)
 {
 	read(client, buffer, length);
 	return 1;
 }
 
-struct dsocketClient make_dsocketClient(char* addr, int port)
+struct dsocket_tcp_client make_dsocket_tcp_client(char* addr, int port)
 {
-	struct dsocketClient output;
+	struct dsocket_tcp_client output;
 	output.addr = addr;
 	output.port = port;
 	return output;
 }
 
-void raze_dsocketClient(struct dsocketClient* sck)
+void raze_dsocket_tcp_client(struct dsocket_tcp_client* sck)
 {
 	//raze_ddString(&(sck->addr));
 }
 
-int dsocketClient_connect(struct dsocketClient* sck)
+int dsocket_tcp_client_connect(struct dsocket_tcp_client* sck)
 {
 	sck->dscr = socket(AF_INET, SOCK_STREAM, 0);
 	if (sck->dscr == -1) return 1;
@@ -109,11 +201,11 @@ int dsocketClient_connect(struct dsocketClient* sck)
 	if (inet_pton(AF_INET, sck->addr, &sck->server.sin_addr) <= 0) return 1;
 	return connect(sck->dscr, (struct sockaddr*)&sck->server, sizeof(sck->server));
 }
-int dsocketClient_send(struct dsocketClient sck, const char* data, long length)
+int dsocket_tcp_client_send(struct dsocket_tcp_client sck, const char* data, long length)
 {
 	return send(sck.dscr, data, length, 0);
 }
-int dsocketClient_receive(struct dsocketClient sck, char* buffer, long length)
+int dsocket_tcp_client_receive(struct dsocket_tcp_client sck, char* buffer, long length)
 {
 	read(sck.dscr, buffer, length);
 	return 1;
